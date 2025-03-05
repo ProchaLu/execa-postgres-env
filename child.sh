2c3085146f81f6cd822afb827e41d178ee812ed9
Lukas Prochazka <prochazka.lu@gmail.com>
2025-03-05 17:35:58 +0100

    Simplify

--- child.sh
+++ child.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 echo "[Postgres] Setting up PostgreSQL on Alpine Linux..."
 
-PGHOST=/postgres-volume/run/postgresql
-PGDATA="$PGHOST/data"
+PGDATA="/postgres-volume/run/postgresql/data"
 
 echo "[Postgres] === ENV_VARS_START ==="
 echo "API_KEY=12345"
@@ -21,22 +20,7 @@ initdb -D "$PGDATA"
 echo "[Postgres] Prepending volume path to Unix Domain Socket path..."
 sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" "$PGDATA/postgresql.conf"
 
-echo "[Postgres] Enabling connections on all available IP interfaces..."
-echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
-
 echo "[Postgres] Starting PostgreSQL with pg_ctl..."
 pg_ctl start -D "$PGDATA"
 
-echo "[Postgres] Checking PostgreSQL status..."
-pg_ctl status -D "$PGDATA"
-
-echo "[Postgres] Creating database, user and schema..."
-psql -U postgres postgres << SQL
-  CREATE DATABASE $PGDATABASE;
-  CREATE USER $PGUSERNAME WITH ENCRYPTED PASSWORD '$PGPASSWORD';3
-  GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSERNAME;
-  \\connect $PGDATABASE
-  CREATE SCHEMA $PGUSERNAME AUTHORIZATION $PGUSERNAME;
-SQL
-
-echo "[Postgres] PostgreSQL setup complete."
+echo "[Postgres] PostgreSQL running..."
